eb7a200d4f7f4524442a1a9bbe6a513b2abc4c32
WilliamAlb <bigw0@yahoo.com>
2021-11-13 10:16:07 +0200

    deleted placeholder

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,7 +6,7 @@ import './Work.css'
 const testitem=[
     {
         name:'Rythmic Gymnastics',
-        description:"A website developed for my mother's rhythmic gymnastics business in Karmi'el, implementing a minimalistic style and a modern feeling all the while attempting to maintain the integrity and personality of both the business itself and the owner, the development process taught me to better implement flex and grid type layouts as well as solidify my knowledge in JavaScript and styling prowess with CSS, all the while utilizing Photoshop and Figma as aids in the construction process, due to this being my first project a substantial amount of idependant problem solving existed",
+        description:"A website developed for my mother's rhythmic gymnastics business in Karmi'el, implementing a minimalistic style and a modern feeling all the while attempting to maintain the integrity and personality of both the business itself and the owner, the development process taught me to better implement flex and grid type layouts as well as solidify my knowledge in JavaScript and styling prowess with CSS, all the while utilizing Photoshop and Figma as aids in the construction process, due to this being my first project a substantial amount of independent problem solving was used alongside teaching myself how to design and build a project from the ground up.",
         stack:['css','html','js'],
         picture:images.rythmicproject
     },
@@ -15,12 +15,6 @@ const testitem=[
         description:'A Javascript project assigned during my course, a small terminal game which involves a grid and a random number of "bombs" spread across it with a single win state objective, the player must navigate through using keyboard stroke inputs to achieve his goal, which is the "hat".',
         stack:['js'],
         picture:images.hatproject
-    },
-    {
-        name:'cool project',
-        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec magna nisi. Phasellus blandit dui porttitor sollicitudin ultrices. Sed sagittis mauris metus, rutrum luctus nunc porta eleifend. Integer ut bibendum enim. Praesent ligula lectus, vehicula non lorem at, fermentum varius sem. Maecenas consequat elit vehicula dictum suscipit. Nullam tortor metus, accumsan eu euismod sit amet, blandit vel ante. Ut tristique justo sit amet metus mattis placerat. Donec blandit maximus leo ut vulputate. Etiam id nulla et quam tristique mollis. Nunc id arcu sem.',
-        stack:['css','html','js'],
-        picture:images.rythmicproject
     }
 ]
 function Work(){
