e169f919ddb3be72d18966f66cfaef08aa2348dd
WilliamAlb <bigw0@yahoo.com>
2021-11-21 02:27:43 +0200

    Added more work array objects

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,6 +5,12 @@ import './Work.css'
 
 const testitem=[
     {
+        name:'Themer',
+        description:"Made with React, Redux and Firebase's platform with authentication and Firestore as a database it is an educational web application made with an aim to offer a solid database of themes for users to access at their leisure, share themes and apply them to their own site to preview or keep on the site for personal preference, once a user is registered and logged in, they may submit a theme to be viewed by others.",
+        stack:['react','redux','js','css','html'],
+        picture:images.themerproject
+    },
+    {
         name:'Rythmic Gymnastics',
         description:"A website developed for my mother's rhythmic gymnastics business in Karmi'el, implementing a minimalistic style and a modern feeling all the while attempting to maintain the integrity and personality of both the business itself and the owner, the development process taught me to better implement flex and grid type layouts as well as solidify my knowledge in JavaScript and styling prowess with CSS, all the while utilizing Photoshop and Figma as aids in the construction process, due to this being my first project a substantial amount of independent problem solving was used alongside teaching myself how to design and build a project from the ground up.",
         stack:['css','html','js'],
--- src/components/utils/images.js
+++ src/components/utils/images.js
@@ -1,11 +1,13 @@
 import eva from '../../images/Eva-alt.jpg';
 import jerry from '../../images/Jerry-alt.png';
 import rythmicproject from '../../images/rythmic-project.jpg'
+import themerproject from '../../images/themer-project.jpg'
 import hatproject from '../../images/hat-project.jpg';
 
 export const images = {
     eva:eva,
     jerry:jerry,
     rythmicproject:rythmicproject,
+    themerproject:themerproject,
     hatproject:hatproject
 }
