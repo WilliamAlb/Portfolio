eff6109752ad0237e87a68077b22e4448b22020c
WilliamAlb <bigw0@yahoo.com>
2021-12-21 01:00:59 +0200

    added intersection observer to render work items

--- src/components/WorkItem.js
+++ src/components/WorkItem.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React,{useRef,useEffect} from "react";
 import { icons } from "./utils/icons";
 
 export function WorkItem(props){
+    const ref = useRef(null);
+
+    useEffect(()=>{
+        const observer = new IntersectionObserver(([entry])=>{
+            if(entry.isIntersecting){
+                ref.current.classList.add('on-screen');
+                observer.unobserve(ref.current);
+            }
+        },{threshold:0.7});
+
+        if(ref.current){
+            observer.observe(ref.current);
+        }
+    },[ref]);
+
     return(
-        <div className={props.right?'work-item-right':'work-item-left'}>
-            <div className="work-img-div"><a href={props.workItem.link} target="_blank" rel="noreferrer"><img src={props.workItem.picture} alt='work example'></img></a></div>
+        <div className={props.right?'work-item-right':'work-item-left'} ref={ref}>
+            <div className="work-img-div"><a href={props.workItem.link} target="_blank" rel="noreferrer"><img src={props.workItem.picture} alt='work example' loading="lazy"></img></a></div>
             <div className={props.right?'content-div-right':'content-div-left'}>
                 <div className={props.right?'header-div-right':'header-div-left'}>
                     <h4>{props.workItem.name}</h4>
